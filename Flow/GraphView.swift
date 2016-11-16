4d44599eeb0b0a114cc1a807d3dbdc89e00f6dca
Stanley Pan <stanleypan.egre@gmail.com>
2016-11-17 00:09:50 +0900

    Create GraphView gradient

--- Flow/GraphView.swift
+++ Flow/GraphView.swift
@@ -7,15 +7,23 @@
 //
 
 import UIKit
+@IBDesignable
 
 class GraphView: UIView {
+    
+    @IBInspectable var startColor: UIColor = .red
+    @IBInspectable var endColor: UIColor = .green
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+        let context = UIGraphicsGetCurrentContext()
+        let colors = [startColor.cgColor, endColor.cgColor]
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let colorLocations: [CGFloat] = [0.0, 1.0]
 
+        let gradient = CGGradient(colorsSpace: colorSpace, colors: colors as CFArray, locations: colorLocations)
+        
+        let startPoint = CGPoint.zero
+        let endPoint = CGPoint(x: 0, y: self.bounds.height)
+        context?.drawLinearGradient(gradient!, start: startPoint, end: endPoint, options: [])
+    }
 }
